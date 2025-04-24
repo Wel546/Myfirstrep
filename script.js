9dc0ad87bf1d75bedcff42af60ce66bee1d7dc89
Wel546 <welisonaraujo8@gmail.com>
2025-04-24 17:21:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let numero1 = Number(prompt("Digite o primeiro número"));
+let numero2 = Number(prompt("Digite o primeiro número"));
+let resultado = numero1 + numero2;
+let conclusao = 'O que significa que você é o amor da minha vida ❤❤'
+document.write('O resultado é: ' + resultado + "<br>");
+document.write(conclusao);
+console.log(resultado);
+//alert(resultado)
+
